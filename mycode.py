ef228bf55b208a18565e787d71266ba45f8f7231
Pranshul Sharma <pranshulsharma83@gmail.com>
2025-08-27 00:48:00 +0530

    Second version of data saved

--- mycode.py
+++ mycode.py
@@ -8,6 +8,10 @@ data = {'Name': ["Alice", 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
+# Adding new row to df for Version2
+new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+df.loc[len(df.index)] = new_row_loc
+
 
 # Ensure the data directory exists at the root level
 data_dir = 'data'
